bf4c2b190bdb8e28f82cdbee5f0fc20742686378
Sushant More <more.13@osu.edu>
2017-11-29 22:37:17 -0500

    Day0 challenge completed

--- src/Day0.java
+++ src/Day0.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class Day0 {
+	
 	public static void main(String []args){
-		System.out.println("hellooo");
+		
+		Scanner scanInput = new Scanner(System.in); // Use scanner class to read from stdin
+		
+		String s = scanInput.nextLine(); // read the input line
+		
+		scanInput.close();
+		
+		System.out.println("Hello, World!");
+		
+		System.out.println(s);
 		
 	}
 }
