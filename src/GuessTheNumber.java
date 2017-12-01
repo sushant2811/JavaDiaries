d7ad58e40e676339f3e0ec5eb779bbe8c9893752
Sushant More <more.13@osu.edu>
2017-12-01 01:57:44 -0500

    Building guessTheNumber game

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,9 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessTheNumber {
 	
+	int theNumber;
+	int max;
+	Scanner scan = new Scanner(System.in);
+	
+	// constructor
+	public GuessTheNumber(){
+		max = 100;
+		Random random = new Random();
+		theNumber = Math.abs(random.nextInt()) % (max + 1);
+	}
+	
+	public void play() {
+		while (true) {
+			int guess = scan.nextInt();
+			if (guess > theNumber){
+				System.out.println("Guess too big. Guess again!");
+			} else if (guess < theNumber){
+				System.out.println("Guess too small. Guess again!");
+			} else {
+				System.out.println("Correct guess!");
+				break;
+			}
+		}
+		
+		
+	}
+	
 	public static void howBigIsTheNumber(int x){
 		if (x >= 0 && x <= 10) {
-			System.out.println("Number pretty small");
+			System.out.println("Number pretty small.");
 		} else if (x >= 11 && x <= 100) {
 			System.out.println("Number pretty big");
 		} else {
@@ -14,14 +43,9 @@ public class GuessTheNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		howBigIsTheNumber(0);
-		howBigIsTheNumber(10);
-		howBigIsTheNumber(11);
-		howBigIsTheNumber(100);
-		howBigIsTheNumber(5);
-		howBigIsTheNumber(75);
-		howBigIsTheNumber(-1);
-		howBigIsTheNumber(105);
+		GuessTheNumber guessGame = new GuessTheNumber();
+		System.out.println("Make a guess between 0 and " + guessGame.max + " inclusive.");
+		guessGame.play();
 	}
 
 }
